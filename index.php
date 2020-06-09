f80c273100982918c83bac0a86f39fd569d15525
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2020-06-09 20:32:30 +0300

    Pridejau nuotraukas

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos libero illum eum quibusdam, fugiat quaerat dolorem deserunt impedit magni ipsum deleniti, earum ratione ab cupiditate amet. Dicta velit natus modi!</p>
 
     <img src="https://picsum.photos/300/200" alt="">
+    <img src="https://picsum.photos/300/200" alt="">
+    <img src="https://picsum.photos/300/200" alt="">
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis velit ipsa doloremque omnis, commodi cum tempore recusandae quam pariatur repellat quae.</p>
 
