2a6a44434632202ad75915960bd51e7323feb974
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2020-06-09 20:15:20 +0300

    Pradinio puslapio keli elementai

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 
 <?php include "header.php" ?>
 
-  
-  <h1>Pagrindinis puslapis</h1>
+  <div class="container">
+
+    <h1>Pagrindinis puslapis</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos libero illum eum quibusdam, fugiat quaerat dolorem deserunt impedit magni ipsum deleniti, earum ratione ab cupiditate amet. Dicta velit natus modi!</p>
+
+    <img src="https://picsum.photos/300/200" alt="">
+
+  </div>
 
 
 <?php include "footer.php" ?>
