8c636675ad2440b8c9cfe19c98c0217e04f6a419
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2020-06-09 20:20:48 +0300

    Tesiu darbus su pagrindiniu puslapiu

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 
   <div class="container">
 
-    <h1>Pagrindinis puslapis</h1>
+    <h1>Mano nerealaus projekto antraštė</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos libero illum eum quibusdam, fugiat quaerat dolorem deserunt impedit magni ipsum deleniti, earum ratione ab cupiditate amet. Dicta velit natus modi!</p>
 
     <img src="https://picsum.photos/300/200" alt="">
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis velit ipsa doloremque omnis, commodi cum tempore recusandae quam pariatur repellat quae.</p>
 
   </div>
 
