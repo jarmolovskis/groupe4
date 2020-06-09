cbde0583ad6d3c924d3abca1d0c6ccc459602b5f
Kazimiras Jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2020-06-09 20:19:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   <div class="container">
 
-    <h1>Pagrindinis puslapis</h1>
+    <h1>Mano antraste</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos libero illum eum quibusdam, fugiat quaerat dolorem deserunt impedit magni ipsum deleniti, earum ratione ab cupiditate amet. Dicta velit natus modi!</p>
 
     <img src="https://picsum.photos/300/200" alt="">
